51802af84c686ea4d21e99259f18a48c3c5a0f44
harshad <Harshadkajale333@gmail.com>
2024-03-17 10:47:58 +0530

    add home section

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect, useState } from "react";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import Wsa from "./Wattson_Strange_Attractor";
